04b524a4beb18a3b535c38ef41d0d474a812ef4d
Caleb Oki <caleb.oki@ibm.com>
2022-03-17 21:57:17 -0400

    added image and description fields

--- Models/Product.model.js
+++ Models/Product.model.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const ProductSchema = new Schema({
+  image: {
+    type: String,
+    required: false
+  },
   name: {
     type: String,
     required: true
+  },
+  description: {
+    type: String,
+    required: false
   },
   price: {
     type: Number,
